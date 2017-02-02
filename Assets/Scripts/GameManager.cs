4219110e85bf427a8b1000634718a638e2c32581
MaxGuidry <mguidry7@outlook.com>
2017-02-02 02:03:13 -0600

    still having trouble spawning bricks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public GameObject ball;
     public GameObject brick;
     private GameObject[] bricks;
     private Transform positionForBricks;
-    
+
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
+        positionForBricks = brick.transform;
+        MakeBricks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void MakeBricks()
+    {
         positionForBricks.position = new Vector3(-8f, 2, 0);
-        for (int i =0;i<10;i++)
+        for (int i = 0; i < 10; i++)
         {
+            Transform newPosition = brick.transform;
+
+            newPosition.position += positionForBricks.position;
             brick.transform.position = positionForBricks.position;
-            //bricks[i] = brick;
-            //bricks[i] = Instantiate(brick);
-            brick = Instantiate(brick) as GameObject;
-            positionForBricks.position += new Vector3(2, 0, 0);
+            Instantiate(brick,positionForBricks.position,Quaternion.identity);
+          
+            positionForBricks.position += new Vector3(2f, 0f, 0f);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    
+    }
+
 }
